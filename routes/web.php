f56affde25dd884546e8c6ad3d8390ce3f37f583
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-10-03 21:28:26 +0800

    routes lesson

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,63 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $person = [
-        'name'=> 'Railley',
-        'email'=> 'railley@gmail.com',
-    ];
-    dd($person);
     return view('welcome');
 });
 
 // route view
 Route::view('/about', 'about');
 
-//route parameters
-Route::get("/product/{id}", function(string $id){
-    return "Product for ID = $id";
-})->whereNumber('id');
+// //route parameters
+// // Route::get("/product/{id}", function(string $id){
+// //     return "Product for ID = $id";
+// // })->whereNumber('id');
+
+// Route::get('/', function(){
+//     $productUrl = route('product.view', ['lang' => 'en', 'id' => 1]);
+//     dd($productUrl);
+//     return view('welcome');
+// });
+
+// Route::get('/user/{username}', function (string $username) {
+//     return "Alpha Username  = $username";
+// })->whereAlpha("username");
+
+// // NAMED ROUTE WITH PARAMETER
+
+// Route::get("/p/{lang}/product/{id}", function (string $lang, string $id) {
+// })->name("product.view");
+
+// Route::get("/user/profile", function (){})->name('profile');
+// Route::get('/current-user', function (){
+//     return to_route('profile');
+// });
+
+// Route Group
+// Route::prefix('admin')->group(function () {
+//     Route::get('users', function () {
+//         return '/admin/users';
+//     });
+// });
+
+// Route::name('admin.')->group(function () {
+//     Route::get('users', function () {
+//         return '/users'; // But the route name is "admin.users"
+//     })->name('users');
+// });
 
 
-Route::get('/user/{username}', function (string $username) {
-    return "Alpha Username  = $username";
-})->whereAlpha("username");
+/// Route adding 2 numbers Challege
+// Route::get('/sum/{num1}/{num2}', function (float $num1, float $num2) {
+//     return $num1 + $num2;
+// })->whereNumber(['num1', 'num2']);
+
+
+
+
+
+
+
+
+Route::fallback(function () {
+    return 'fallback';
+});
