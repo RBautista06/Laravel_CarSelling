612b0f51fd91da96c7e2cea7d3b400bcff8f9ddc
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-10-03 19:20:38 +0800

    route parameters, view, route varible specification

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $person = [
+        'name'=> 'Railley',
+        'email'=> 'railley@gmail.com',
+    ];
+    dd($person);
     return view('welcome');
 });
+
+// route view
+Route::view('/about', 'about');
+
+//route parameters
+Route::get("/product/{id}", function(string $id){
+    return "Product for ID = $id";
+})->whereNumber('id');
+
+
+Route::get('/user/{username}', function (string $username) {
+    return "Alpha Username  = $username";
+})->whereAlpha("username");
